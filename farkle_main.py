28519bd7a55d5ca597244b710adde8b06613d04d
Confused404 <116277026+Confused404@users.noreply.github.com>
2023-12-22 16:42:42 -0500

    gi

---
+++ farkle_main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import time
+
+def main():
+    root = Tk()
+    root.title("Farkle")
+    root.geometry("1000x1000")
+    root.configure(background = "white")
+    root.resizable(width = False, height = False)
+    root.mainloop()
+
+main()
